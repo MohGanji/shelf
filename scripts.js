64500f0a4418f0d10b611fc0b5cbb6f6933767f1
Mohammad macbook <m_ganji@sfu.ca>
2022-06-16 01:31:18 -0700

    add template for books

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ class Util {
      * @param {string} name 
      */
     static fmt(name) {
-        return name.toLowerCase().replace(' ', '-')
+        return name.toLowerCase().replaceAll(' ', '-')
     }
 
     /**
@@ -28,6 +28,24 @@ const Templates  = {
         <div class="item-part item-subtitle">${subtitle}</div>
         <a   class="item-part item-link" href="http://${website}">${website}</a>
         <div class="item-part item-date">Written on: ${date}</div>
+    </div>
+</div>
+`;
+        return Util.strToHtml(templateString);
+    },
+    book: (title, subtitle, author, recommendTo, date, link, rating) => {
+        let templateString = `
+<div class="item clickable" onclick="window.location.href = './${Util.fmt(title)}.html'">
+    <div class="col img-container">
+        <img class="item-image" src="/assets/books/${Util.fmt(title)}.jpg" width="100%" height="auto" />
+        <div class=""><b>${rating}</b>/10</div>
+    </div>
+    <div class="col other-container">
+        <div class="item-part item-title">${title} - by ${author}</div>
+        <div class="item-part item-subtitle">${subtitle}</div>
+        <div class="item-part item-date"><b>Who I would recommend to:</b> ${recommendTo}</div>
+        <div class="item-part item-date">Date read: ${date}</div>
+        <a   class="item-part item-link" href="http://${link}">Amazon page</a>
     </div>
 </div>
 `;
