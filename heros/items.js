dedf613b10fb35919a70caf95983322afa6bdfcb
Mohammad macbook <m_ganji@sfu.ca>
2022-07-06 23:46:48 -0700

    comment out heros from the list

--- heros/items.js
+++ heros/items.js
@@ -1,22 +1,28 @@
 const heros = [
-    {
-        title: 'Derek Sivers',
-        subtitle: 'A curious minimalist adventurer with exceptional prose writing ability, and radical mindset and counter-intuitive useful ideas.',
-        date: '2022-06-05',
-        website: 'sive.rs'
-    },
-    {
-        title: 'Tim Urban',
-        subtitle: 'A curious blogger. His blog posts contain stick figure comics for better illustration.',
-        date: '2022-06-05',
-        website: 'waitbutwhy.com'
-    },
-    {
-        title: 'Jia Jiang',
-        subtitle: 'An ordinary immigrant who overcame fear of rejection with a 100 day rejection therapy challenge. His talks are inspiring because he shares his vulnerability easily.',
-        date: '2022-06-07',
-        website: 'rejectiontherapy.com'
-    }
+    // {
+    //     title: 'Derek Sivers',
+    //     subtitle: `
+    //         A curious minimalist adventurer with exceptional prose writing ability, and radical mindset and counter-intuitive useful ideas.
+    //     `,
+    //     date: '2022-06-05',
+    //     website: 'sive.rs'
+    // },
+    // {
+    //     title: 'Tim Urban',
+    //     subtitle: `
+    //         A curious blogger. His blog posts contain stick figure comics for better illustration.
+    //     `,
+    //     date: '2022-06-05',
+    //     website: 'waitbutwhy.com'
+    // },
+    // {
+    //     title: 'Jia Jiang',
+    //     subtitle: `
+    //         An ordinary immigrant who overcame fear of rejection with a 100 day rejection therapy challenge. His talks are inspiring because he shares his vulnerability easily.
+    //     `,
+    //     date: '2022-06-07',
+    //     website: 'rejectiontherapy.com'
+    // }
 ]
 let items = heros.map((hero) => Templates.hero(hero.title, hero.subtitle, hero.date, hero.website))
 let containerElement = document.getElementById('items-container')
