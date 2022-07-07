358490b28975225729d0edcb792d8c3a27058307
Mohammad macbook <m_ganji@sfu.ca>
2022-07-07 02:07:00 -0700

    add subtitle for the courage to be disliked

--- library/items.js
+++ library/items.js
@@ -2,25 +2,26 @@ const books = [
     {
         title: 'The Courage to Be Disliked',
         subtitle: `
-            some stuff I want to say about this book
-            in three lines 
-            some
+            I love this books because it combines philosophy with psychology in a story format.
+            It's a dialouge between a wise old man and an inexperienced curious young person.
+            It expands on the Adlerian psychology, on how there is always an internal purpose that justifies our actions(or inactions).
+            It provides controversial and eye-opening views on self-worth, confidence, fear, trauma, superiority, subjectiveness of our worlds, and the meaning of community.
         `,
         author: 'Ichiro Kishimi',
-        link: '...',
+        link: 'https://www.amazon.ca/Courage-Be-Disliked-Phenomenon-Happiness/dp/1501197274',
         recommendTo: 'everyone',
         rating: 10,
         date: '2021-12',
     },
-    {
-        title: 'Essentialism',
-        subtitle: 'A curious minimalist adventurer with exceptional prose writing ability, and radical mindset and counter-intuitive useful ideas.',
-        author: 'Greg McKeown',
-        link: '',
-        recommendTo: 'everyone',
-        rating: 10,
-        date: '2020-06',
-    },
+    // {
+    //     title: 'Essentialism',
+    //     subtitle: 'A curious minimalist adventurer with exceptional prose writing ability, and radical mindset and counter-intuitive useful ideas.',
+    //     author: 'Greg McKeown',
+    //     link: '',
+    //     recommendTo: 'everyone',
+    //     rating: 10,
+    //     date: '2020-06',
+    // },
 ]
 let items = books.map((b) => Templates.book(b.title, b.subtitle, b.author, b.recommendTo, b.date, b.link, b.rating))
 let containerElement = document.getElementById('items-container')
