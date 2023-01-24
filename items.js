894b67b246e6934dccc54176bb35ebb0295a0d05
Mohammad macbook <m_ganji@sfu.ca>
2023-01-24 13:53:54 -0800

    add grahams number

--- items.js
+++ items.js
@@ -1,20 +1,26 @@
 const posts = [
-    {
-        title: "Do it 100 times first, then we'll talk",
-        url: '100',
-        subtitle: '',
-        date: '2023-01',
-    },
-    {
-        title: "Need a hug? Read `The boy, the mole, the fox and the horse`",
-        url: 'boy mole fox horse',
-        subtitle: '',
-        date: '2023-01',
-    },
-    {
-        title: 'Tired of reading this blog? Discover new ones with `BlogFrog`',
-        url: 'blogfrog',
-        subtitle: `
+  {
+    title: 'Math can be fun, they said',
+    url: 'grahams-number',
+    subtitle: '',
+    date: '2023-01',
+  },
+  {
+    title: "Do it 100 times first, then we'll talk",
+    url: '100',
+    subtitle: '',
+    date: '2023-01',
+  },
+  {
+    title: 'Need a hug? Read `The boy, the mole, the fox and the horse`',
+    url: 'boy mole fox horse',
+    subtitle: '',
+    date: '2023-01',
+  },
+  {
+    title: 'Tired of reading this blog? Discover new ones with `BlogFrog`',
+    url: 'blogfrog',
+    subtitle: `
             I was working, after a half hour or so, I unconciously opened a new tab, typed in: "twitter.com".
             Next thing I know is I'm doomscrolling down my twitter feed for an hour.
             This is me every day. 
@@ -30,35 +36,35 @@ const posts = [
             I let the button take me somewhere random on the internet. I read one blog post, learn something, and I get back to work. 
             Because there is no infinite scrolling feed after that.
         `,
-        link: 'blogfrog.xyz',
-        date: '2022-07',
-    },
-    {
-        title: 'Afraid of what people might think? Read `The Courage to Be Disliked`',
-        url: 'The Courage to Be Disliked',
-        subtitle: `
+    link: 'blogfrog.xyz',
+    date: '2022-07',
+  },
+  {
+    title: 'Afraid of what people might think? Read `The Courage to Be Disliked`',
+    url: 'The Courage to Be Disliked',
+    subtitle: `
             This book combines philosophy with psychology in a story format.
             It's a dialouge between a wise old man and an inexperienced curious young person.
             It expands on the Adlerian psychology, on how there is always an internal purpose that justifies our actions(or inactions).
             It provides controversial and eye-opening views on self-worth, confidence, fear, trauma, superiority, subjectiveness of our worlds, and the meaning of community.
         `,
-        date: '2021-12',
-    },
-    {
-        title: 'Why you should start a blog',
-        url: 'Why you should start a blog',
-        subtitle: `
+    date: '2021-12',
+  },
+  {
+    title: 'Why you should start a blog',
+    url: 'Why you should start a blog',
+    subtitle: `
         Have you ever thought why you follow so many people on social media? 
         Maybe you share their interests, or maybe you just enjoy seeing other people do normal things. 
         Thinking is also a normal thing we all do, and thoughts can also be interesting...
         `,
-        date: '2021-11',
-    }
-    // title: engaging, teasing title, should be enough to get a click.
-    // url: the last portion of the url, should be short, one or two word thingy, memorable.
-    // subtitle, will not appear here, but will be used for other socials for engagement.
-    // date: just to have a list of how frequently I'm writing.
+    date: '2021-11',
+  },
+  // title: engaging, teasing title, should be enough to get a click.
+  // url: the last portion of the url, should be short, one or two word thingy, memorable.
+  // subtitle, will not appear here, but will be used for other socials for engagement.
+  // date: just to have a list of how frequently I'm writing.
 ]
 let items = posts.map((post) => Templates.blogpost(post.title, post.url, post.subtitle, post.date))
 let containerElement = document.getElementById('items-container')
-items.forEach(item => containerElement.appendChild(item))
+items.forEach((item) => containerElement.appendChild(item))
